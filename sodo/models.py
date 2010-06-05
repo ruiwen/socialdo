bcaa013ce764295828c7f34bbcb963d7a1886130
Ruiwen Chua <ruiwen@ubuntu-vm.(none)>
2010-06-05 21:21:32 +0800

    Tweaks to SvarmDo models
    User
     - 'collaborators' renamed 'contacts'
     - Added lists() method which returns either the only the lists owned by the User, or including those of which User is a collaborator
    
    List
     - 'user' renamed 'owner'
     - Added 'desc' field for extended descriptions of Lists
     - Added 'collaborators' field to easily retrieve list of Users sharing this List
     - Added utility methods items_completed() / items_incomplete() / overall_progress() to get metrics on overall state of completion of list
     - Added utility method accept() to bring incoming Items into List
    
    Item
     - Removed 'parent_list'
     - Defined 'primary_list' and 'secondary_lists' for fine-grained management of shared items

--- sodo/models.py
+++ sodo/models.py
@@ -3,6 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.db import models
 from django.contrib.auth.models import User, UserManager
 import os
+from itertools import chain
          
 def get_image_path(instance, filename):
 	return os.path.join('users', instance.id, filename) 
@@ -23,10 +24,18 @@ def get_image_path(instance, filename):
 class User(User):
 	"""Custom User model, extending Django's default User"""
 	profile_image = models.ImageField(upload_to=get_image_path, blank=True)
-	collaborators = models.ManyToManyField("self", symmetrical=False, blank=True)
+	contacts = models.ManyToManyField("self", symmetrical=False, blank=True)
 	
 	objects = UserManager()
 	
+	def lists(self, with_shared=True):
+		if with_shared:
+			#return self.owned_lists.all() + self.shared_lists.all()
+			# TODO: lists() should return lists in order of last used
+			return list(chain(self.owned_lists.all(), self.shared_lists.all()))
+		else:
+			return self.owned_lists.all()
+		
 	def __unicode__(self):
 		if self.first_name and self.last_name:
 			return self.get_full_name()
@@ -37,10 +46,44 @@ class User(User):
 
 class List(models.Model):
 	parent_list = models.ForeignKey('List', blank=True, null=True, related_name="sub_lists")
-	user = models.ForeignKey(User, related_name="lists")
+	owner = models.ForeignKey(User, related_name="owned_lists")
 	date_added = models.DateTimeField(auto_now_add=True)
 	date_modified = models.DateTimeField(auto_now=True)
 	name = models.CharField(max_length=255, blank=False)
+	desc = models.CharField(max_length=1024, blank=True, null=True)
+	collaborators = models.ManyToManyField(User, related_name="shared_lists", blank=True)	
+	
+	
+	def items_completed(self, with_secondary=True):
+		completed = self.primary_items.filter(completed=True)
+		
+		if with_secondary:
+			completed += self.secondary_items.filter(completed=True)
+			
+		return completed
+	
+	
+	def items_incomplete(self, with_secondary=True):
+		incomplete = self.primary_items.filter(completed=False)
+		
+		if with_secondary:
+			incomplete += self.secondary_items.filter(completed=False)
+			
+		return incomplete
+	
+	
+	def overall_progress(self, with_secondary=True):
+		completed = self.items_completed(with_secondary)
+
+		total = self.primary_items.all()
+		if with_secondary:
+			total += self.secondary_items.all()
+		
+		return int( (float(completed.count()) / float(total.count())) * 100 )
+	
+	
+	def accept(self, item):
+		self.secondary_items.add(item.id)
 	
 	def __str__(self):
 		return self.name
@@ -53,7 +96,8 @@ class Item(models.Model):
 	completed = models.BooleanField(default=False)
 	user = models.ForeignKey(User, related_name="items")
 	desc = models.CharField(max_length=255)
-	parent_list = models.ForeignKey(List, related_name="list_items")
+	primary_list = models.ForeignKey(List, related_name="primary_items")
+	secondary_lists = models.ManyToManyField(List, related_name="secondary_items", blank=True)
 	date_added = models.DateField(auto_now_add=True)
 	date_modified = models.DateField(auto_now=True)
 	date_completed = models.DateTimeField(null=True, blank=True)
